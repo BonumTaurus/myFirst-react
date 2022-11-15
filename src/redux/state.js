d72a409e62129d46bdd7f8892441695224d7f287
BonumTaurus <dimazostap@gmail.com>
2022-11-15 12:14:30 +0200

    new

---
+++ src/redux/state.js
@@ -0,0 +1,27 @@
+
+let state = {
+	profilePage: {
+		users: [
+			{ like: 3, name: 'Andry', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDATwYwwNGQUhEBH7V8QlkfxJbSVkOjx_Jtg&usqp=CAU' },
+			{ like: 17, name: 'Viola', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkNClHcBngc-qOfx6OQ-rCm6L-xHp-t6R2QA&usqp=CAU' },
+			{ like: 10, name: 'Sasha', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAoi6Z0xOdcuffZzWjB-I6DwyPs-y834ZUCw&usqp=CAU' },
+			{ like: 34, name: 'Anton', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAtsz7BUlaNJEAa3OuDWaR6ny2XQWUhrYpLQ&usqp=CAU' }
+		],
+	},
+
+	messagesPage: {
+		friends: [
+			{ id: 1, name: 'Alex', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJu9OerVpHkWSPio5jM8qH9jYdV-fgIE5eGg&usqp=CAU' },
+			{ id: 2, name: 'Dima', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-37GQvmKz3WlZ8WCjhBvpjfCsjmy1L4itfA&usqp=CAU' },
+			{ id: 3, name: 'Sandra', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBbsMEFO-izKuFoNmwhh3tZuS-2l1quAPJ_Q&usqp=CAU' },
+			{ id: 4, name: 'Tony', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfDpH3994qXDAPD99gt4RBvO9N7lVf8KCgxw&usqp=CAU' },
+			{ id: 5, name: 'Sonya', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx2BodWXtn4vibrb1IVha8p8L3qCJUmfqe3A&usqp=CAU' },
+			{ id: 6, name: 'Alex', ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJpygmSfVyLNBtm-CH3ejLNQ5k0kh_OtAjmQ&usqp=CAU' },
+		],
+		dialogs: [
+			{ frases: ['Hi friend!', ' How are you?', ' How are your react?'] },
+		]
+	}
+}
+
+export default state;
